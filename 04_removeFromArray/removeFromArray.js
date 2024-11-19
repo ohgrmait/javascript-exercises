5fdae6358441a4ecfd708e0b476fa9e684edcc2f
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-11-19 16:55:42 +0530

    Add JS based on the "Arrays and Loops" lesson

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+  let newArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    if (numbers.includes(array[i])) {
+      continue;
+    }
+    newArray.push(array[i]);
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (start < 0 || end < 0) {
+    return 'ERROR';
+  }
 
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    return 'ERROR';
+  }
+
+  let sum = 0;
+  
+  let min = Math.min(start, end);
+  let max = Math.max(start, end);
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
